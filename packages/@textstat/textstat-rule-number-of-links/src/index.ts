8c97ce52acb1f66f29ba718eafd2d0c085fb5234
azu <azuciao@gmail.com>
2018-10-31 00:58:04 +0900

    feat(package): add textstat-rule-number-of-links

---
+++ packages/@textstat/textstat-rule-number-of-links/src/index.ts
@@ -0,0 +1,28 @@
+// LICENSE : MIT
+"use strict";
+import { TextstatRuleReporter } from "@textstat/rule-context";
+
+const report: TextstatRuleReporter = function(context) {
+    const { Syntax, report } = context;
+    let count = 0;
+    return {
+        [Syntax.Document]() {
+            count = 0;
+        },
+        [Syntax.Link]() {
+            count++;
+        },
+        [Syntax.Document + ":exit"](node) {
+            report(node, {
+                message: "Number of links in the document",
+                data: [
+                    {
+                        name: "number of Links",
+                        value: count
+                    }
+                ]
+            });
+        }
+    };
+};
+export default report;
---
+++ packages/@textstat/textstat-rule-number-of-links/test/snapshots/5/input.md
@@ -0,0 +1,7 @@
+![image](./image.move)
+
+[link](https://example.com/a)
+[link](https://example.com/b)
+[link](https://example.com/c)
+[link](https://example.com/d)
+[link](https://example.com/e)
