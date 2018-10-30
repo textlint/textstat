85514a5212f9800875f9fe40fffab826ec5a09e4
azu <azuciao@gmail.com>
2018-10-31 01:07:34 +0900

    feat(package): add textstat-rule-number-of-paragraphs

---
+++ packages/@textstat/textstat-rule-number-of-paragraphs/src/index.ts
@@ -0,0 +1,28 @@
+// LICENSE : MIT
+"use strict";
+import { TextstatRuleReporter } from "@textstat/rule-context";
+
+const report: TextstatRuleReporter = function(context) {
+    const { Syntax, report } = context;
+    let count = 0;
+    return {
+        [Syntax.Document]() {
+            count = 0;
+        },
+        [Syntax.Paragraph]() {
+            count++;
+        },
+        [Syntax.Document + ":exit"](node) {
+            report(node, {
+                message: "Number of paragraphs in the document",
+                details: [
+                    {
+                        name: "Number of paragraphs",
+                        value: count
+                    }
+                ]
+            });
+        }
+    };
+};
+export default report;
---
+++ packages/@textstat/textstat-rule-number-of-paragraphs/test/snapshots/5/input.md
@@ -0,0 +1,9 @@
+This is 1.
+
+This is 2.
+
+This is 3.
+
+This is 4.
+
+This is 5.
